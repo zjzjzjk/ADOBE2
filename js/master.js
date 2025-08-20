f420f5128e6e56e971e69b638725bd1dec43e22e
zjzjzjk <pp0926760593@gmail.com>
2025-08-20 21:34:47 +0800

    upgrade

---
+++ js/master.js
@@ -0,0 +1,13 @@
+// const confrig = 
+
+
+$('[data-fancybox]').fancybox({
+    protect:true ,
+    loop:true ,
+    transitionEffect: 'rotate',
+    // infobar: true,
+    // toolbar: false,
+    buttons:['zoom' , 'share' ,'fullScreen' , 'close'] ,
+})
+
+// transitionEffect: 'rotate' 調整變化
